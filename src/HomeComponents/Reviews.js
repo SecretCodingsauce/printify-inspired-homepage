e11603b31a283c6c35e39441eba86be855881f71
Ayushman <redsteel200.as@gmail.com>
2024-09-21 07:12:49 +0530

    fixed a bug

--- src/HomeComponents/Reviews.js
+++ src/HomeComponents/Reviews.js
@@ -6,16 +6,16 @@ export default function Reviews({comments}){
         <section id="reviews" className="py-16 md:py-20 text-center">
         <div className="container mx-auto px-4">
           <h3 className="text-2xl md:text-3xl font-semibold">What Our Customers Say</h3>
-
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10">
      {comments.map((comment)=>(
-         <div key={comment.id} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10">
-         <div className="p-6 bg-white shadow-md rounded-lg">
+         
+         <div key={comment.id} className="p-6 bg-white shadow-md rounded-lg">
            <p className="text-gray-600">{comment.body}</p>
            <h4 className="mt-4 font-bold">{comment.email}</h4>
          </div>
-         </div>
+        
      ))}
-
+</div>
            
           </div>
       
